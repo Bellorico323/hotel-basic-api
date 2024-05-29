f7f1b6257d3a1ed5cf98cccd2f244c99c0af358d
Bellorico323 <mabo.orico@gmail.com>
2024-05-28 21:49:43 -0300

    CORS

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,17 @@ import fastify from 'fastify'
 import { roomRoutes } from './routes/room/routes'
 import { reservationRoutes } from './routes/reservations/routes'
 import { guestsRoutes } from './routes/guests/routes'
+import cors from '@fastify/cors'
 
 export const app = fastify()
 
 app.register(roomRoutes)
 app.register(reservationRoutes)
 app.register(guestsRoutes)
+
+app.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+})
