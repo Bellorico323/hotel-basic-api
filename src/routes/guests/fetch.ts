04e30bd7faf46f188cfafd1e7a2dec366ad24c7b
Bellorico323 <mabo.orico@gmail.com>
2024-06-03 21:22:25 -0300

    add created at and order items by date

--- src/routes/guests/fetch.ts
+++ src/routes/guests/fetch.ts
@@ -2,7 +2,11 @@ import { prisma } from '@/lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function fetch(request: FastifyRequest, reply: FastifyReply) {
-  const guests = await prisma.guest.findMany()
+  const guests = await prisma.guest.findMany({
+    orderBy: {
+      createdAt: 'asc',
+    },
+  })
 
   return reply.status(200).send({ guests })
 }
--- src/routes/reservations/fetch.ts
+++ src/routes/reservations/fetch.ts
@@ -2,7 +2,11 @@ import { prisma } from '@/lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function fetch(request: FastifyRequest, reply: FastifyReply) {
-  const reservations = await prisma.reservation.findMany()
+  const reservations = await prisma.reservation.findMany({
+    orderBy: {
+      createdAt: 'asc',
+    },
+  })
 
   return reply.status(200).send({ reservations })
 }
